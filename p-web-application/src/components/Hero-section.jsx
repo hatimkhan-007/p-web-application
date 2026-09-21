9fbd5e5f18df966c519f8eac7d32118906150471
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-21 21:33:44 +0500

    Adding the main page headings

--- p-web-application/src/components/Hero-section.jsx
+++ p-web-application/src/components/Hero-section.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Hero_section() {
   return (
-    <div>Hero-section</div>
+    <div>
+      <h1><b>In a world full of bold ideas, we turn ambition into meaningful results.</b></h1>
+      <h1><b>A company built on innovation, refined through experience, with one clear vision: Build What’s Next.</b></h1>
+  </div>
   )
 }
 
