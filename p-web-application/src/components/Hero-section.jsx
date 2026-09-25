bcea536778c6ee02f545f25e2685ada7517733b6
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-25 18:53:05 +0500

    Adding the raw structure of the hero section

--- p-web-application/src/components/Hero-section.jsx
+++ p-web-application/src/components/Hero-section.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Hero_section() {
   return (
     <div>
-      <h1><b>In a world full of bold ideas, we turn ambition into meaningful results.</b></h1>
-      <h1><b>A company built on innovation, refined through experience, with one clear vision: Build What’s Next.</b></h1>
+      <h1 className='h-h1'><b>In a world full of bold ideas, we turn ambition into meaningful results.</b></h1>
+      <h1 className='h-h1'><b>A company built on innovation, refined through experience, with one clear vision: Build What’s Next.</b></h1>
+      <button className='btn btn-primary'>Save Time, Lead the World.</button>
   </div>
   )
 }
