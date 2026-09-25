0743894a03c5aaf002d83b6734f89ca419298abd
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-25 18:56:04 +0500

    Adding the css file in jsx

--- p-web-application/src/components/Hero-section.jsx
+++ p-web-application/src/components/Hero-section.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './Hero-section.css';
 
 function Hero_section() {
   return (
-    <div>
+    <div className='hero-section'>
       <h1 className='h-h1'><b>In a world full of bold ideas, we turn ambition into meaningful results.</b></h1>
       <h1 className='h-h1'><b>A company built on innovation, refined through experience, with one clear vision: Build What’s Next.</b></h1>
       <button className='btn btn-primary'>Save Time, Lead the World.</button>
