0b8dd15e856c2d499d4a6c9d1a19a8f934871451
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-25 19:52:32 +0500

    Adding css file for clients section.

--- p-web-application/src/App.jsx
+++ p-web-application/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Hero_section from './components/Hero-section.jsx'
+import Clients from './components/Clients.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero_section />
+      <Clients />
     </>
   )
 }
---
+++ p-web-application/src/components/Clients.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { List } from 'react-bootstrap-icons'
+
+List = ["Client 1", "Client 2", "Client 3", "Client 4", "Client 5", "Client 6", "Client 7", "Client 8", "Client 9", "Client 10"];
+
+function Clients() {
+  return (
+    <div className='client-section'>
+        <h1>Clients</h1>
+        {/* <div className='client-list'>
+            {List.map((client, index) => (
+                <div key={index} className='client-item'>
+                    <List size={24} />
+                    <span>{client}</span>
+                </div>
+            ))}
+        </div> */}
+    </div>
+  )
+}
+
+export default Clients
