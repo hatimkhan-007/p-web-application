281b384bb460726589edfc61e70d588584b5af73
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-25 19:18:22 +0500

    Adding professional css

--- p-web-application/src/components/Hero-section.jsx
+++ p-web-application/src/components/Hero-section.jsx
@@ -4,11 +4,13 @@ import './Hero-section.css';
 function Hero_section() {
   return (
     <div className='hero-section'>
-      <h1 className='h-h1'><b>In a world full of bold ideas, we turn ambition into meaningful results.</b></h1>
-      <h1 className='h-h1'><b>A company built on innovation, refined through experience, with one clear vision: Build What’s Next.</b></h1>
-      <button className='btn btn-primary'>Save Time, Lead the World.</button>
+      <div className="inner-s">
+        <h1 className='h-h1'><b>In a world full of bold ideas, we turn ambition into meaningful results.</b></h1>
+        <h1 className='h-h2'><b>A company built on innovation, refined through experience, with one clear vision: Build What’s Next.</b></h1>
+        <button className='btn btn-primary'>Save Time, Lead the World.</button>
+      </div>
   </div>
   )
 }
 
-export default Hero-section
+export default Hero_section
