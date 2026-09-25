d957e16864c6d4ef75feceedfe2fca628a85bcb2
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-25 19:00:35 +0500

    Adding the basic css

--- p-web-application/src/App.jsx
+++ p-web-application/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar.jsx'
+import Hero_section from './components/Hero-section.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero_section />
     </>
   )
 }
